f603b650ceb6dcebbddbc52af1de703bbf293ffd
TyAlbao <161251535+TyAlbao@users.noreply.github.com>
2026-03-24 22:44:02 -0700

    credentials for streamlit bigquery

--- bigquery_utils.py
+++ bigquery_utils.py
@@ -2,16 +2,25 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 import streamlit as st
 
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(project="master-trackman-project", credentials=credentials)
+
+@st.cache_resource
+def get_bigquery_client():
+    credentials = service_account.Credentials.from_service_account_info(
+        dict(st.secrets["gcp_service_account"])
+    )
+    return bigquery.Client(
+        project="master-trackman-project",
+        credentials=credentials,
+    )
+
 
 def import_data(dataset, table_name):
+    client = get_bigquery_client()
+
     query = f"""
-    SELECT GameID, Inning, Top_Bottom, PAofInning, PitchofPA, Batter, PlateLocSide, PlateLocHeight, PitchCall, Balls, Strikes, delta_run_exp
+    SELECT *
     FROM `master-trackman-project.{dataset}.{table_name}`
     ORDER BY GameID ASC, Inning ASC, Top_Bottom DESC, PAofInning ASC, PitchofPA ASC
     """
-    df = client.query(query).to_dataframe()
-    return df
+
+    return client.query(query).to_dataframe()
